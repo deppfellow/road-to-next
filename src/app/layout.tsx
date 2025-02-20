713c84649887a0dabcb5eb59688b4ad726672d08
raidrafif <raid.rafif@sigma.co.id>
2025-02-20 16:58:07 +0700

    chore: Add button and card components. Refactor the ticket cards to use ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Button, buttonVariants } from '@/components/ui/button';
 import './globals.css';
 import { homePath, ticketsPath } from '@/route';
 import type { Metadata } from 'next';
@@ -30,10 +31,17 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b bg-background/95 px-8 py-2.5 backdrop-blur">
-          <Link href={homePath()} className="text-lg font-bold">
-            Home
+          <Button asChild>
+            <Link href={homePath()} className="text-lg font-bold">
+              Home
+            </Link>
+          </Button>
+          <Link
+            href={ticketsPath()}
+            className={buttonVariants({ variant: 'outline' })}
+          >
+            Tickets
           </Link>
-          <Link href={ticketsPath()}>Tickets</Link>
         </nav>
         <main className="flex min-h-screen flex-1 flex-col overflow-y-auto overflow-x-hidden bg-secondary/20 px-8 py-16 md:px-48">
           {children}
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,9 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { initialTickets } from '@/data';
-import { cn } from '@/lib/utils';
 import { ticketPath } from '@/route';
 import clsx from 'clsx';
 import Link from 'next/link';
 import React from 'react';
+
+const TicketIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
+      />
+    </svg>
+  );
+};
 
 export default function TicketsPage() {
   return (
@@ -13,31 +39,45 @@ export default function TicketsPage() {
         <p className="text-sm text-muted-foreground">Available ticket:</p>
       </div>
 
-      <div className="flex flex-1 flex-col items-center gap-y-4 md:grid md:grid-cols-5 md:gap-4">
+      <div className="animate-fade-in-from-top flex flex-1 flex-col items-center gap-y-4 md:grid md:grid-cols-4 md:gap-4">
         {initialTickets.map((ticket) => {
           return (
-            <div
-              key={ticket.id}
-              className="w-full max-w-[420px] rounded border border-slate-700 p-4"
-            >
-              <h3
-                className={clsx('truncate text-lg font-semibold', {
-                  'text-slate-500 line-through': ticket.status === 'Done',
-                })}
-              >
-                {ticket.title}
-              </h3>
-              <p
-                className={clsx('truncate text-sm', {
-                  'line-through': ticket.status === 'Done',
-                })}
-              >
-                {ticket.content}
-              </p>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                See detail
-              </Link>
-            </div>
+            <Card key={ticket.id} className="w-full max-w-[420px]">
+              <CardHeader>
+                <CardTitle className="flex gap-x-2">
+                  <TicketIcon />
+                  <span
+                    className={clsx('truncate text-lg font-semibold', {
+                      'text-slate-500 line-through': ticket.status === 'Done',
+                    })}
+                  >
+                    {ticket.title}
+                  </span>
+                </CardTitle>
+              </CardHeader>
+
+              <CardContent>
+                <span
+                  className={clsx(
+                    'line-clamp-3 whitespace-break-spaces text-sm',
+                    {
+                      'line-through': ticket.status === 'Done',
+                    }
+                  )}
+                >
+                  {ticket.content}
+                </span>
+              </CardContent>
+
+              <CardFooter>
+                <Link
+                  href={ticketPath(ticket.id)}
+                  className="text-sm underline"
+                >
+                  See detail
+                </Link>
+              </CardFooter>
+            </Card>
           );
         })}
       </div>
