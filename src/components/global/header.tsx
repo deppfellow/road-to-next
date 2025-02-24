8eb3c701ce3415d4acbede70e658b58cc55fedf8
raidrafif <raid.rafif@sigma.co.id>
2025-02-24 16:33:45 +0700

    chore: more dark mode

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <>
       <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b bg-background/95 px-8 py-2.5 backdrop-blur">
-        <div>
+        <div className="align-items flex gap-x-2">
           <Button asChild variant="ghost">
             <Link href={homePath()}>
               <HardHat size={48} />
@@ -17,7 +17,7 @@ const Header = () => {
           </Button>
         </div>
 
-        <div className="flex gap-x-2">
+        <div className="align-items flex gap-x-2">
           <ThemeSwitcher />
           <Link
             href={ticketsPath()}
--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { LucideMoon, LucideSun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import { Button } from '@/components/ui/button';
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
@@ -14,7 +14,7 @@ const ThemeSwitcher = () => {
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <LucideSun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <LucideMoon className="h-4 w-4 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
+      <LucideMoon className="absolute h-4 w-4 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
     </Button>
   );
 };
